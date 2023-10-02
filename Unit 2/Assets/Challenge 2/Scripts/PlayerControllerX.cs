9fee0271704d911afe5ff4468c043221d83ef14e
B00150644 <B00150644@mytudublin.ie>
2023-10-02 20:31:36 +0100

    Finished Challenge

---
+++ Unit 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    float spawnCooldown = 0.5f;
+    float timeSinceSpawn = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceSpawn += Time.deltaTime;
+        //when pressing spacebar , send dog if more time than the cooldown has gone
+        if (Input.GetKeyDown(KeyCode.Space) && timeSinceSpawn > spawnCooldown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timeSinceSpawn = 0.0f;
+        }
+    }
+}
